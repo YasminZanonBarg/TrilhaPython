7d5c3eb56a2c2b09f5617c3faf05ef43dfd9c5b5
Yasmin Zanon Barg <yasmin.barg@hotmail.com>
2025-12-08 11:36:31 -0300

    content of sixth class - libs

---
+++ dia_06/04_modulos_lib.py
@@ -0,0 +1,14 @@
+# Importando biblioteca
+import math
+
+# Use o "." para acessar os métodos da biblioteca. Exemplos:
+math.sqrt(9) 
+math.pow(2, 4)
+math.e
+math.pi
+
+# é uma boa prática a importação de apenas as funções que estão sendo utilizadas
+from math import pi, sqrt
+
+# posso também ao importar uma biblioteca colocar um apelido na função
+import math as mt
---
+++ dia_06/exercicios/02_exercicios_libs.py
@@ -0,0 +1,7 @@
+#%% Gerando um número aleatório
+import random
+random.randint(1, 10)
+
+#%% Escolhendo aleatóriamente o nome em uma lista
+x = ["Téo", "Maria",  "Jose", "Ana", "João"]
+random.choice(x)
